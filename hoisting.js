eb937aefdfdfcdc5fae88ee7db504296a053115a
TanyaIndian <97530331+TanyaIndian@users.noreply.github.com>
2024-01-26 23:23:10 +0530

    added more functions

--- hoisting.js
+++ hoisting.js
@@ -8,3 +8,45 @@ function hello(){
 console.log(hello);
 const hello = "hello world";
 console.log(hello);
+
+
+// functions inside function 
+function app(){
+    const myFunc = () =>{
+        console.log("hello from myFunc")
+    }
+    
+    const addTwo = (num1, num2) =>{
+        return num1 + num2;
+    }
+
+    const mul = (num1, num2) => num1* num2;
+
+    console.log("inside app");
+    myFunc();
+    console.log(addTwo(2,3));
+    console.log(mul(2,3));
+}
+app();
+
+
+// lexical scope 
+const myVar = "value1";
+
+function myApp(){
+    
+
+    function myFunc(){
+        // const myVar = "value59";
+        const myFunc2 = () =>{
+            console.log("inside myFunc", myVar);
+        }
+        myFunc2();
+    }
+
+
+    console.log(myVar);
+    myFunc();
+}
+
+myApp();

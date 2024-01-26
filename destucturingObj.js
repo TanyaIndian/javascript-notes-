52e1f912ac95f1a7887dce1aa2444d52b37fee1b
TanyaIndian <97530331+TanyaIndian@users.noreply.github.com>
2024-01-26 22:59:24 +0530

    functions

---
+++ destucturingObj.js
@@ -0,0 +1,23 @@
+// object destructuring
+const band = {
+    bandName: "led zepplin",
+    famousSong: "stairway to heaven",
+    year: 1968,
+    anotherFamousSong: "kashmir",
+  };
+  
+  let { bandName, famousSong, ...restProps } = band;
+  console.log(bandName);
+  console.log(restProps);
+
+  // nested destructuring 
+const users = [
+    {userId: 1,firstName: 'harshit', gender: 'male'},
+    {userId: 2,firstName: 'mohit', gender: 'male'},
+    {userId: 3,firstName: 'nitish', gender: 'male'},
+]
+
+const [{firstName: user1firstName, userId}, , {gender: user3gender}] = users;
+console.log(user1firstName);
+console.log(userId);
+console.log(user3gender);

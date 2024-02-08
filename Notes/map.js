02b70ec9a6c732c79447746a61c1f510f4fb5790
TanyaIndian <97530331+TanyaIndian@users.noreply.github.com>
2024-02-08 23:04:38 +0530

    added till maps

---
+++ Notes/map.js
@@ -0,0 +1,46 @@
+// In JavaScript, an iterable is an object that implements the iterable protocol, which means it has a defined way 
+// to retrieve its elements sequentially, typically via an iterator.Iterables are objects that can be iterated over, also can use size or length
+// ex- array,string,map,set,argument array
+// meaning you can loop through their elements.
+
+// / n JavaScript, the Map object is a built-in data structure that allows you to store key-value pairs where both the keys and the values can be of any data type. Maps are similar to objects but provide some advantages, such as maintaining the insertion order of elements and 
+// allowing any type of value as a key.
+
+let map = new Map();
+map.set("key1", "value1");
+map.set("key2", "value2");
+map.set([1,2,3,4],'abc')
+console.log(map)
+
+console.log(map.get("key1")); // Output: "value1"
+
+// Checking Existence:
+// You can check if a key exists in the Map using the has method.
+console.log(map.has("key1")); // Output: true
+map.delete("key1");
+
+
+map.forEach((value, key) => {
+    console.log(key, value);
+  });
+  
+  // Alternatively, using a for...of loop
+  for (let [key, value] of map) {
+    console.log(key, value);
+  }
+
+  
+  console.log(map.size); // Output: 1
+
+  map.clear();
+
+
+//   let map = new Map();
+
+// Adding entries
+
+
+// Updating value for an existing key
+map.set("key1", "new value");
+
+console.log(map.get("key1")); // Output: "new value"
---
+++ Notes/sets.js
@@ -0,0 +1,34 @@
+// In JavaScript, a Set is a built-in object that allows you to store unique values of any type. 
+// Sets maintain the insertion order of elements, which means the order of elements in the Set corresponds to the order in which they were added.
+let set = new Set();
+set.add(1);
+set.add("Hello");
+set.add({ name: "John" })
+console.log((set));
+// Set(4) { 1, 'Hello', { name: 'John' }, { name: 'John' } }
+
+console.log(set.size); // Output: 3
+
+set.delete(1);
+
+set.clear();
+
+console.log(set.has(1)); // Output: true
+console.log(set.has(5)); // Output: false
+
+set.forEach(function(value) {
+    console.log(value);
+  });
+  
+  // Alternatively, using for...of loop
+  for (let value of set) {
+    console.log(value);
+  }
+//   Converting Set to Array:
+let array = Array.from(set);
+
+// Using Sets for Unique Values:
+let numbers = [1, 2, 3, 4, 4, 5, 5];
+let uniqueNumbers = new Set(numbers);
+console.log(uniqueNumbers); // Output: Set { 1, 2, 3, 4, 5 }
+
